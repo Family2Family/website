a40a24a853871e1d14ef78c9e3d1974bedba6b15
Josue Lopez <josueal1@uci.edu>
2020-02-29 14:06:35 -0800

    feature(app): common-base-html & fixed links

--- wsgi.py
+++ wsgi.py
@@ -4,24 +4,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index.html")
-def index():
-  return render_template("index.html")
-@app.route("/features.html")
-def features():
-  return render_template("features.html")
-@app.route("/pricing.html")
-def pricing():
-  return render_template("pricing.html")
-@app.route("/contact.html")
-def contact():
-  return render_template("contact.html")
-@app.route("/donate.html")
-def blog():
-  return render_template("donate.html")
 
+@app.route("/")
+@app.route("/about")
+def index():
+    return render_template("about.html")
+
+
+@app.route("/timeline")
+def timeline():
+    return render_template("timeline.html")
+
+
+@app.route("/donate")
+def about():
+    return render_template("donate.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/vision")
+def donate():
+    return render_template("vision.html")
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
